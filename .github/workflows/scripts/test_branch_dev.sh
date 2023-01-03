f2f44b978e24a2af1e6d4ac464e8355cc7e3ee1f
Slava <egslava@gmail.com>
2023-01-03 08:55:10 +0300

    fixup

--- .github/workflows/scripts/test_branch_dev.sh
+++ .github/workflows/scripts/test_branch_dev.sh
@@ -1,17 +1,26 @@
-python3 -m pip install git+https://"$USER":"$TOKEN"@github.com/"$USER"/sublesee.git@dev#subdirectory=soft
+function on_exit() {
+  rm -rf $GENERATED
+}
+trap "on_exit" EXIT
+
 
 # smoke tests
-BASE=.github/workflows/scripts/
+BASE=.github/workflows/scripts
 DATA=$BASE/data
-sublesee srt2xlsx $DATA/1.Eng.srt
-sublesee xlsx2srt $DATA/1.Eng.srt.xlsx
-diff -ZbB -c \
+GENERATED=$DATA/generated
+mkdir $GENERATED
+sublesee srt2xlsx $DATA/1.Eng.srt $GENERATED/1.xlsx
+sublesee xlsx2srt $GENERATED/1.xlsx $GENERATED/1.srt
+
+# can't handle BOM, so I just removed it from
+# the test file
+diff -bB -C 3 \
   $DATA/1.Eng.srt \
-  $DATA/1.Eng.srt.xlsx.srt\
+  $GENERATED/1.srt\
   || {
     echo "xlsx2srt failed to restore the original srt"\
           " file. See the diff above";
     exit 1;
   }
 
-sublesee srt2html $DATA/1.Eng.srt
+sublesee srt2html $DATA/1.Eng.srt $GENERATED/1.html
