92fad3fb10d19a94b5bb75f2c7b685cc4d1005ed
Slava <egslava@gmail.com>
2023-01-03 08:10:51 +0300

    fixup

--- .github/workflows/scripts/test_branch_dev.sh
+++ .github/workflows/scripts/test_branch_dev.sh
@@ -1,12 +1,14 @@
 python3 -m pip install git+https://"$USER":"$TOKEN"@github.com/"$USER"/sublesee.git@dev#subdirectory=soft
 
 # smoke tests
-sublesee srt2xlsx ./data/1.Eng.srt
-sublesee xlsx2srt ./data/1.Eng.srt.xlsx
+BASE=.github/workflows/scripts/
+DATA=$BASE/data
+sublesee srt2xlsx $DATA/1.Eng.srt
+sublesee xlsx2srt $DATA/1.Eng.srt.xlsx
 cmp --silent\
-  1.Eng.srt\
-  1.Eng.srt.xlsx.srt\
+  $DATA/1.Eng.srt\
+  $DATA/1.Eng.srt.xlsx.srt\
   || { echo "xlsx2srt failed to restore the "\
             "original srt file"; exit 1; }
 
-sublesee srt2html ./data/1.Eng.srt
+sublesee srt2html $DATA/1.Eng.srt
