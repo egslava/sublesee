270cf11046cbdd4dc32297b93bccd76b0c45998e
Slava <egslava@gmail.com>
2023-01-04 15:31:33 +0300

    changed ci/cd tests to use temp files

--- soft/tests/test_parse.py
+++ soft/tests/test_parse.py
@@ -5,9 +5,9 @@ import pytest
 from unittest import TestCase
 from glob import glob
 
-import os
+from os import path
 from tempfile import gettempdir, TemporaryFile, \
-    NamedTemporaryFile
+    NamedTemporaryFile, TemporaryDirectory
 
 from sublesee.io import read_srt, write_xslx, \
     read_xlsx, write_srt
@@ -15,27 +15,34 @@ from sublesee.io import read_srt, write_xslx, \
 FILENAMES = glob('tests/subs/*')
 
 
+@pytest.fixture()
+def tempdir():
+    with TemporaryDirectory() as dir:
+        yield dir
+    pass
+
+
 def test_read_srt():
     df = read_srt('tests/subs/1.Eng.srt')
-    with TemporaryFile() as file:
-        write_xslx(file, df)
+    with TemporaryFile() as outfile:
+        write_xslx(outfile, df)
 
 
-def test_write_srt():
+def test_write_srt(tempdir: str):
     df = read_xlsx('tests/trans/1.Eng.srt.xlsx')
-    with NamedTemporaryFile() as file:
-        write_srt(file.name, df)
+    write_srt(path.join(tempdir, 'out.srt'), df)
 
 
-def test_integration():
-    with NamedTemporaryFile() as xlsx, \
-            NamedTemporaryFile() as srt:
+def test_integration(tempdir: str):
+    srt_name = path.join(tempdir, 'out.srt')
+
+    with NamedTemporaryFile() as xlsx:
         write_xslx(xlsx.name,
                    read_srt('tests/subs/1.Eng.srt'))
 
-        write_srt(srt.name, read_xlsx(xlsx.name))
+        write_srt(srt_name, read_xlsx(xlsx.name))
         srt_before = pysrt.open('tests/subs/1.Eng.srt')
-        srt_after = pysrt.open(srt.name)
+        srt_after = pysrt.open(srt_name)
         assert srt_before == srt_after
 
 
